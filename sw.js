791f10ba9715ac57c0d75471a2582658a4c2ecf9
joaopedrozo <98230250+joaopedrozo@users.noreply.github.com>
2026-03-04 21:48:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pedrozo-v26';
+const CACHE = 'pedrozo-v27';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
